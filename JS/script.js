8bd5826bf07684ada8bcef9fe6c50a5308de6e80
Bakinson Babajide <93973753+Jide-nna@users.noreply.github.com>
2022-09-28 17:19:11 +0100

    progressing

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,4 @@
-var xmlhttp = new XMLHttpRequest()
+ /*  xmlhttp = new XMLHttpRequest()
 var url = "http://127.0.0.1:5500//data.json"
 xmlhttp.open("GET", url,true)
 xmlhttp.send()
@@ -7,12 +7,10 @@ xmlhttp.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
         var data = JSON.parse(this.responseText)
         // console.log(data)
-        day = data.day_population.map(function(elem) {
-            return elem.day
-        })
-        console.log(day)
+        var data = JSON.parse(day)
+        console.log(data)
     }
-}
+}*/
  let myChart = document.getElementById('canvas').getContext('2d');
 
   let massSpendChart = new Chart(myChart, {
@@ -22,7 +20,14 @@ xmlhttp.onreadystatechange = function() {
         labels: ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'],
         datasets:[{
             label: 'Amount',
-            data:["200",'100','400', '700', '800', '900', '800'
+            data:[
+             "17.45",
+            '34.91',
+            '52.36', 
+            '31.07',
+            '23.39', 
+            '43.28', 
+            '25.48'
             ],
             backgroundColor:[
                 'hsl(10, 79%, 65%)',
@@ -33,17 +38,45 @@ xmlhttp.onreadystatechange = function() {
                 'hsl(10, 79%, 65%)',
                 'hsl(10, 79%, 65%)'
             ],
-            borderRadius: 8,
+            borderRadius: 5,
         }]
     },
-    options:{}
-})
-/* 
     options: {
-        scales: false,
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                enabled: false,
+              },
+           /* tooltip: {
+                mode: 'index',
+                intersect: false
+                }, */
+        },
+      /*  hover: {
+            mode: 'nearest',
+            intersect: false
+            }, */
+        scales: {
+            x: {
+                display: true,
+                borderWidth: 0,
+                lineWidth: 0,
+                grid:{
+                display: false,
+                }
+             },
             y: {
-                beginAtZero: true
-            } 
-        }
-}); */
+                display: false,
+                grid:{
+                display: false,
+                }
+          },
+        },
+        tooltips: {
+            enabled: false,
+          },
+    }
+});
        
